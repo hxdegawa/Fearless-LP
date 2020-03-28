b2802c324cdf5a2c4596d919e9f3a3963e5eaece
Hiro Degawa <hxdegawa@gmail.com>
2020-03-28 18:31:18 +0900

    Modified OGP URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
       { name: 'twitter:card', content: 'summary_large_image' },
       {
         name: 'twitter:image',
-        content: 'https://new.fearlessf.com/ogp.png'
+        content: 'http://new.fearlessf.com/ogp.png'
       },
       {
         name: 'twitter:description',
@@ -37,7 +37,7 @@ module.exports = {
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://new.fearlessf.com/'
+        content: 'http://new.fearlessf.com/'
       },
       { hid: 'og:title', property: 'og:title', content: 'FEARLESS' },
       {
@@ -49,7 +49,7 @@ module.exports = {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://new.fearlessf.com/ogp.png'
+        content: 'http://new.fearlessf.com/ogp.png'
       }
     ],
     link: [
